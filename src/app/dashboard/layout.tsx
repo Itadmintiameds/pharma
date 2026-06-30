45cc23b70ff1654e471dcbfec150e84c68b41486
ShreerakshaRao <your_github_shreeraksha@tiameds.ai>
2026-06-30 10:07:04 +0530

    Configured Sidebar, Navbar and Global css files

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Sidebar from "../components/layout/Sidebar";
+import Navbar from "../components/layout/Navbar";
+
+export default function layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+
+      <div className="flex flex-1 flex-col">
+        <Navbar />
+
+        <main className="flex-1 overflow-y-auto p-6">{children}</main>
+      </div>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Noto_Sans, Work_Sans, Open_Sans } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const notoSans = Noto_Sans({
   subsets: ["latin"],
+  variable: "--font-noto",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const workSans = Work_Sans({
   subsets: ["latin"],
+  variable: "--font-work",
+});
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-open-sans",
 });
 
 export const metadata: Metadata = {
@@ -23,11 +28,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className={`${notoSans.variable} ${workSans.variable} ${openSans.variable}`}>
+      <body>{children}</body>
     </html>
   );
 }
